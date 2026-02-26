6e216aaf5fc19df0a9e26f57aa66d47d9c99aa00
Robiya <jurayevarobiya60@gmail.com>
2026-02-26 19:37:27 +0500

    3-normativ: Custom QuerySet

--- books/models.py
+++ books/models.py
@@ -10,3 +10,26 @@ class Book(models.Model):
         return self.title
 
 
+
+class BaseQuerySet(models.QuerySet):
+    def delete(self):
+        return super().update(is_deleted=True)
+
+
+class DeletedManager(models.Manager):
+    def get_queryset(self):
+        return BaseQuerySet(self.model, using=self._db).filter(is_deleted=False)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    objects = DeletedManager()
+    all_objects = models.Manager()
+
+    def __str__(self):
+        return self.title
